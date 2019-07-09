16d2f9bd36f0545cb1d422742318fe7901309123
Alex Gondek <alexgondek@gmail.com>
2019-07-09 11:51:42 +0200

    Add exit code and exit time to fish_title

--- home/.config/fish/functions/fish_title.fish
+++ home/.config/fish/functions/fish_title.fish
@@ -1,4 +1,6 @@
 function fish_title
+  set -l last_status $status
   set -l home_escaped (echo -n $HOME | sed 's/\//\\\\\//g')
-  echo -n $PWD | sed "s/^$home_escaped/~/" | sed 's/ /%20/g'
+  set -l date_finished (date '+%T')
+  echo -n "$PWD (exit $last_status @ $date_finished)" | sed "s/^$home_escaped/~/"
 end
