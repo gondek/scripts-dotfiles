e33148376e165639e5e54352f8ab0054adb00b60
Alex Gondek <alexgondek@gmail.com>
2021-01-01 20:06:56 -0800

    Added Powerline to fish shell

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,5 +1,9 @@
+set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
+
 set -x PATH $PATH ~/bin
 set -x PATH $PATH ~/.n/bin
 set -x PATH $PATH ~/.cargo/bin
 
 set -x fish_greeting
+
+powerline-setup
